e70a28e6e67970b0f3d4683ed9fa1ed4607e5c12
細見元気 <hosomi2001@gmail.com>
2025-01-02 00:30:57 +0900

    textfieldも修正

--- src/component/ions/textfield/DefaultTextField.Ions.tsx
+++ src/component/ions/textfield/DefaultTextField.Ions.tsx
@@ -1,44 +1,9 @@
-import React, { ChangeEvent } from 'react';
+import React from 'react';
 import TextFieldAtoms from '../../atoms/textfield/TextField.Atoms';
+import { TextFieldAtomsProps } from '../../atoms/textfield/TextField.type';
 
-interface TextFieldProps {
-  label: string;
-  value: string;
-  onChange: (
-    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>,
-  ) => void;
-  placeholder?: string;
-  error: string | null;
-  max?: number;
-  type?: string;
-  input_class_name?: string;
-  rows?: number;
-}
-
-const DefaultTextFieldIons: React.FC<TextFieldProps> = ({
-  label,
-  value,
-  onChange,
-  placeholder = '',
-  error,
-  max,
-  type = 'text',
-  input_class_name,
-  rows,
-}) => {
-  return (
-    <TextFieldAtoms
-      label={label}
-      value={value}
-      onChange={onChange}
-      placeholder={placeholder}
-      error={error}
-      max={max}
-      type={type}
-      rows={rows}
-      input_class_name={input_class_name}
-    />
-  );
+const DefaultTextFieldIons: React.FC<TextFieldAtomsProps> = (props) => {
+  return <TextFieldAtoms {...props} />;
 };
 
 export default DefaultTextFieldIons;
