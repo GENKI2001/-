476be3da71e22a8158b6700adb96cedac0674bda
細見元気 <hosomi2001@gmail.com>
2025-01-04 13:47:10 +0900

    ions層のpopupをメモ化した。

--- src/component/ions/popup/DefaultPopup.Ions.tsx
+++ src/component/ions/popup/DefaultPopup.Ions.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import PopupAtoms from '../../atoms/popup/Popup.Atoms';
 import { PopupAtomsProps } from '../../atoms/popup/Popup.type';
 
-const DefaultPopupIons: React.FC<PopupAtomsProps> = (props) => {
+const DefaultPopupIons: React.FC<PopupAtomsProps> = React.memo((props) => {
   return <PopupAtoms {...props} />;
-};
+});
 
 export default DefaultPopupIons;
