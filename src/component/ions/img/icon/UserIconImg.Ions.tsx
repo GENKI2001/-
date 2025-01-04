59736771bd2674a4bb73f3e9bb39e0903e1b30f1
細見元気 <hosomi2001@gmail.com>
2025-01-04 13:44:44 +0900

    ions層のimgのメモ化を行った。

--- src/component/ions/img/icon/UserIconImg.Ions.tsx
+++ src/component/ions/img/icon/UserIconImg.Ions.tsx
@@ -7,19 +7,20 @@ interface ImgProps {
   alt?: string;
   size?: 'small' | 'medium' | 'large';
 }
-const UserIconImgIons: React.FC<ImgProps> = ({
-  user_name,
-  img_src,
-  alt,
-  size = 'medium',
-}) => {
-  return img_src ? (
-    <img src={img_src} alt={alt} className={`icon-img icon-img-ions-${size}`} />
-  ) : (
-    <div className={`icon-img icon-img-ions-${size} user-icon-text-ions`}>
-      {user_name[0]}
-    </div>
-  );
-};
+const UserIconImgIons: React.FC<ImgProps> = React.memo(
+  ({ user_name, img_src, alt, size = 'medium' }) => {
+    return img_src ? (
+      <img
+        src={img_src}
+        alt={alt}
+        className={`icon-img icon-img-ions-${size}`}
+      />
+    ) : (
+      <div className={`icon-img icon-img-ions-${size} user-icon-text-ions`}>
+        {user_name[0]}
+      </div>
+    );
+  },
+);
 
 export default UserIconImgIons;
