5ea7f663c0c3dcce34d5fe0c332ded7098731d4c
細見元気 <hosomi2001@gmail.com>
2025-01-04 13:46:23 +0900

    ions層のpopoverをメモ化した。

--- src/component/ions/popover/DefaultPopover.Ions.tsx
+++ src/component/ions/popover/DefaultPopover.Ions.tsx
@@ -1,8 +1,9 @@
+import React from 'react';
 import PopoverAtoms from '../../atoms/popover/Popover.Atoms';
 import { PopoverAtomsProps } from '../../atoms/popover/Popover.type';
 
-const DefaultPopoverIons: React.FC<PopoverAtomsProps> = (props) => {
+const DefaultPopoverIons: React.FC<PopoverAtomsProps> = React.memo((props) => {
   return <PopoverAtoms {...props} />;
-};
+});
 
 export default DefaultPopoverIons;
