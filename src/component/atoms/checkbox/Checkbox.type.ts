55beb3b2458163bd1a355cf5f19e7d12fca720f4
細見元気 <hosomi2001@gmail.com>
2025-01-08 15:38:32 +0900

    checkbox

--- src/component/atoms/checkbox/Checkbox.type.ts
+++ src/component/atoms/checkbox/Checkbox.type.ts
@@ -1,6 +1,9 @@
+export type CheckboxSizeType = 'small' | 'medium' | 'large';
+
 export type CheckboxAtomsProps = {
   label: string;
   checked: boolean;
   onChange: () => void;
   disabled?: boolean;
+  size?: CheckboxSizeType;
 };
