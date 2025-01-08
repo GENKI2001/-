3328893c450426f1921f2a91f509f4c9136e69f1
細見元気 <hosomi2001@gmail.com>
2025-01-08 15:42:56 +0900

    progressのリファクタリング

--- src/component/atoms/progress/ProgressBar.Atoms.tsx
+++ src/component/atoms/progress/ProgressBar.Atoms.tsx
@@ -1,29 +1,28 @@
 import React from 'react';
 import './ProgressBar.Atoms.css';
+import { ProgressAtomsProps } from './ProgressBar.type';
 
-interface ProgressProps {
-  progress: number;
-}
+const ProgressBarAtoms: React.FC<ProgressAtomsProps> = React.memo(
+  ({ progress, size = 'medium', width }) => {
+    const clampedProgress = Math.min(Math.max(progress, 0), 100);
 
-const ProgressBarAtoms: React.FC<ProgressProps> = React.memo(({ progress }) => {
-  const clampedProgress = Math.min(Math.max(progress, 0), 100);
-
-  return (
-    <div className="progress-container">
-      <div
-        className="progress-bar"
-        role="progressbar"
-        aria-valuenow={clampedProgress}
-        aria-valuemin={0}
-        aria-valuemax={100}
-      >
+    return (
+      <div className="progress-container" style={{ width: width }}>
         <div
-          className="progress-fill"
-          style={{ width: `${clampedProgress}%` }}
-        />
+          className={`progress-bar ${size}`}
+          role="progressbar"
+          aria-valuenow={clampedProgress}
+          aria-valuemin={0}
+          aria-valuemax={100}
+        >
+          <div
+            className="progress-fill"
+            style={{ width: `${clampedProgress}%` }}
+          />
+        </div>
       </div>
-    </div>
-  );
-});
+    );
+  },
+);
 
 export default ProgressBarAtoms;
