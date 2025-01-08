25eebac619bb9bd43676b603e6743b40d71fb687
細見元気 <hosomi2001@gmail.com>
2025-01-08 15:55:24 +0900

    radioボタン

--- src/component/ions/radio/RadioButton.Ions.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import RadioButtonAtoms from '../../atoms/radio/RadioButton.Atoms';
-import { RadioButtonAtomsProps } from '../../atoms/radio/RadioButton.type';
-
-const RadioButtonIons: React.FC<RadioButtonAtomsProps> = React.memo((props) => {
-  return <RadioButtonAtoms {...props} />;
-});
-
-export default RadioButtonIons;
